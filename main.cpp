8a54ddf6eecce2a85832e0590da2a7ec6c9c49b2
hhhannahmmmontana <rubakhin.edu@niuitmo.ru>
2024-12-01 23:06:30 +0300

    refactor: small refactors 2

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@ int main() {
     for (int i = 0; i < 10; ++i) {
         fib.insert(i);
     }
-    kstmc::fibonacci_heap<int>::iterator it = fib.begin();
-    kstmc::fibonacci_heap<int>::iterator end = fib.end();
     for (int i : fib) {
         std::cout << i << std::endl;
     }
